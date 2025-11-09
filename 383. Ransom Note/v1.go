7c1022a524b665fd5d3440fe9580122764936e69
Lyahov Stepan <lyahov.stepa@gmail.com>
2025-11-09 22:37:49 +0300

    [383] solved

---
+++ 383. Ransom Note/v1.go
@@ -0,0 +1,18 @@
+func canConstruct(ransomNote string, magazine string) bool {
+    magazineMap := make(map[rune]int32)    
+
+    for _, r := range magazine {
+        magazineMap[r] = magazineMap[r] + 1
+    }
+    
+    for _, r := range ransomNote {
+        cnt, ok := magazineMap[r]
+        if !ok || cnt <= 0 {
+            return false
+        }
+
+        magazineMap[r] = cnt - 1
+    }
+
+    return true
+}
